2677983b632b46941b272e4f71f63faf7153c000
chibi2412 <103920544+chibi2412@users.noreply.github.com>
2022-04-25 13:44:42 +0900

    Update ApplicationStart.sh

--- ApplicationStart.sh
+++ ApplicationStart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /tmp/CodeDeployExample
-sudo service start nginx
+sudo service start nginx > nginx_start.txt
 
 echo "The ApplicationStart lifecycle event successfully completed." > application-start.txt
