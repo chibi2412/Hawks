723a76582700a502c6808f794a2454d0eedcb5a7
chibi2412 <103920544+chibi2412@users.noreply.github.com>
2022-04-25 13:45:15 +0900

    Update ApplicationStop.sh

--- ApplicationStop.sh
+++ ApplicationStop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /tmp/CodeDeployExample
-sudo service nginx stop
+sudo service nginx stop > stop_nginx.txt
 
 echo "The ApplicationStop lifecycle event successfully completed." > application-stop.txt
